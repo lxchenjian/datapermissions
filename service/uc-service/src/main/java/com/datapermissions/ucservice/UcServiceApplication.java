7947510175c5e1a5af35afebbfd352f0c47a048a
lxchenjian <982861129@qq.com>
2023-05-09 10:40:51 +0800

    nacos看不到服务列表

--- service/uc-service/src/main/java/com/datapermissions/ucservice/UcServiceApplication.java
+++ service/uc-service/src/main/java/com/datapermissions/ucservice/UcServiceApplication.java
@@ -2,8 +2,10 @@ package com.datapermissions.ucservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UcServiceApplication {
 
     public static void main(String[] args) {
